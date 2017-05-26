3df314aeb3a209aacaabda3864fbb0f3ccf68121
Brian Matejek <bmatejek@gmail.com>
2017-05-26 17:33:24 -0400

    javascript: javascript now takes in a Uint8Array

--- src/javascript/handler.js
+++ src/javascript/handler.js
@@ -22,19 +22,20 @@ SEGMENTATION.File.prototype.onError = function(e) {
 
 SEGMENTATION.File.prototype.onLoad = function(e) {
     if (e.target.readyState == FileReader.DONE) {
-        var buffer = e.target.result;
-
-        var bytes = new Uint32Array(buffer);
+        var buffer = new Uint8Array(e.target.result);
 
         // get the time
         var start_time = (new Date()).getTime();
 
         // call COMPRESSO algorithm
-        var data = COMPRESSO.Decompress(bytes);
+        var data = COMPRESSO.Decompress(buffer);
 
         // output the time taken
         total_time = ((new Date()).getTime() - start_time) / 1000
         console.log((data.zres * data.yres * data.xres * 8) / total_time / Math.pow(2, 20));
+
+        var compressed_data = COMPRESSO.Compress(data.decompressed_data, data.zres, data.yres, data.xres, 1, 8, 8);
+
 
         var canvas = document.createElement("canvas");
         canvas.width = data.xres;
@@ -44,7 +45,7 @@ SEGMENTATION.File.prototype.onLoad = function(e) {
         var iz = 4;
         for (var ix = 0; ix < data.xres; ++ix) {
             for (var iy = 0; iy < data.yres; ++iy) {
-                component = data.decompressed_data[IndicesToIndex(ix, iy, iz)];
+                component = compressed_data[IndicesToIndex(ix, iy, iz)];
 
                 red = (Math.trunc(107 * component) % 700) % 255;
                 green = (Math.trunc(509 * component) % 900) % 255;
